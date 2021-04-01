6873f9bf14d4c5746c80f7d37fff8a84f9f69676
KayJuno <dangdan2807@gmail.com>
2021-04-02 00:15:33 +0700

    update: full tuan7

--- BaiTap/tuan7.sql
+++ BaiTap/tuan7.sql
@@ -280,8 +280,8 @@ end
 	else
 		begin
 	update MDepartment
-			set NumOfEmployee = @slNV
-			where DepartmentID = @DepartID
+		set NumOfEmployee = @slNV
+		where DepartmentID = @DepartID
 end
 go
 
@@ -294,6 +294,7 @@ values(291, 'Nguyen', 'Hoang', 'Anh', 1),
 select *
 from MDepartment
 
+drop trigger dbo.kiemTraSLNV;
 drop table dbo.MEmployees
 drop table dbo.MDepartment
 GO
@@ -314,22 +315,20 @@ GO
 -- Freight) VALUES ( 2 ,3, 261, 1652, 4 ,GETDATE() ,GETDATE() , 44594.55,
 -- ,3567.564, ,1114.8638 );
 
-CREATE trigger CreditRating_trigger on [Purchasing].[PurchaseOrderHeader]
+CREATE trigger Purchasing.CreditRating_trigger on [Purchasing].[PurchaseOrderHeader]
 for insert
 AS
-	DECLARE @CRating int, @vendor int
-		SELECT @vendor = i.VendorID
-		FROM inserted as i
-
-		select @CRating = pv.CreditRating
-		from [Purchasing].[Vendor] as pv
-		where @vendor = pv.BusinessEntityID
-
-	if (@CRating = 5)
+	if exists (
+		select *
+from [Purchasing].[PurchaseOrderHeader] as p join inserted as i
+	on p.PurchaseOrderID = i.PurchaseOrderID join [Purchasing].[Vendor] as v
+	on p.VendorID = v.BusinessEntityID
+where v.CreditRating = 5
+	)
 		BEGIN
-			print N'không cho phép chèn'
-			rollback
-		END
+	RAISERROR ('A vendor''s credit rating is too low to accept new purchase orders.', 16, 1);
+	ROLLBACK TRANSACTION
+END
 GO
 
 INSERT INTO Purchasing.PurchaseOrderHeader
@@ -337,7 +336,7 @@ INSERT INTO Purchasing.PurchaseOrderHeader
 VALUES
 	( 2, 3, 261, 1652, 4 , GETDATE() , GETDATE() , 44594.55, 3567.564, 1114.8638 )
 
-drop trigger dbo.CreditRating_trigger
+drop trigger Purchasing.CreditRating_trigger
 GO
 
 -- 5. Viết một trigger thực hiện trên bảng ProductInventory (lưu thông tin số lượng sản 
@@ -351,25 +350,178 @@ GO
 -- thông báo “Kho hết hàng” và đồng 
 -- thời hủy giao tác.
 
+-- tạo bảng MProduct
+create table MProduct
+(
+	MProductID int not null primary key,
+	ProductName nvarchar(50),
+	ListPrice money
+)
+
+insert MProduct
+	(MProductID, ProductName,ListPrice)
+select [ProductID], [Name], [ListPrice]
+from [Production].[Product]
+where [ProductID]<=710
+select*
+from MProduct
+
+-- tạo bảng MSalesOrderHeader
+create table MSalesOrderHeader
+(
+	MSalesOrderID int not null primary key,
+	OrderDate datetime
+)
+
+insert MSalesOrderHeader
+select [SalesOrderID], [OrderDate]
+from [Sales].[SalesOrderHeader]
+where [SalesOrderID] in (select [SalesOrderID]
+from [Sales].[SalesOrderDetail]
+where [ProductID]<=710)
+select*
+from MSalesOrderHeader
+
+-- tạo bảng MSalesOrderDetail
+create table MSalesOrderDetail
+(
+	SalesOrderDetailID int IDENTITY(1,1) primary key,
+	ProductID int not null foreign key(ProductID) references MProduct(MProductID),
+	SalesOrderID int not null foreign key (SalesOrderID) references MSalesOrderHeader(MSalesOrderID),
+	OrderQty int
+)
+insert MSalesOrderDetail
+	(ProductID, SalesOrderID,OrderQty)
+select [ProductID], [SalesOrderID], [OrderQty]
+from [Sales].[SalesOrderDetail]
+where [ProductID] in(select MProductID
+from MProduct)
+
+-- tạo bảng MProduct_inventory
+create table MProduct_inventory
+(
+	productID int not null primary key,
+	quantity smallint
+)
+
+insert MProduct_inventory
+select [ProductID], sum([Quantity]) as sumofquatity
+from [Production].[ProductInventory]
+group by [ProductID]
+go
+
+-- tạo trigger
+create trigger bai5 on MSalesOrderDetail
+for insert
+AS
+begin
+	DECLARE @Qty int, @Quantity int, @productID int
+	select @qty = i.OrderQty, @productID = i.ProductID
+	from inserted i
+
+	select @Quantity =  p.Quantity
+	FROM MProduct_inventory p
+	WHERE @productID = p.ProductID
+
+	if(@Quantity > @Qty)
+	begin
+		update MProduct_inventory
+				set quantity = @Quantity - @Qty
+				where productID = @productID
+	end
+	ELSE if (@Quantity = 0)
+	begin
+		print N'Kho hết hàng'
+		rollback
+	end
+end
+GO
+
+select*
+from MSalesOrderDetail
+select*
+from MSalesOrderHeader
+select*
+from MProduct_inventory
+where [ProductID]=708
+
+---thuc thi trigger
+delete from [MSalesOrderDetail]
+insert [dbo].[MSalesOrderDetail]
+values(708, 43661, 300)
+
+drop trigger bai5
+drop table [MSalesOrderDetail]
+drop table [MSalesOrderHeader]
+drop table [MProduct_inventory]
 
 -- 6. Tạo trigger cập nhật tiền thưởng (Bonus) cho nhân viên bán hàng SalesPerson, khi 
 -- người dùng chèn thêm một record mới trên bảng SalesOrderHeader, theo quy định 
 -- như sau: Nếu tổng tiền bán được của nhân viên có hóa đơn mới nhập vào bảng 
 -- SalesOrderHeader có giá trị >10000000 thì tăng tiền thưởng lên 10% của mức 
 -- thưởng hiện tại. Cách thực hiện:
---  Tạo hai bảng mới M_SalesPerson và M_SalesOrderHeader
+--  Tạo hai bảng mới M_SalesPerson và M_SalesOrderHeader
 -- create table M_SalesPerson 
 -- (
--- SalePSID int not null primary key, 
--- TerritoryID int,
--- BonusPS money
+-- 		SalePSID int not null primary key, 
+-- 		TerritoryID int,
+-- 		BonusPS money
 -- )
 -- create table M_SalesOrderHeader 
 -- (
--- SalesOrdID int not null primary key, 
--- OrderDate date,
--- SubTotalOrd money,
--- SalePSID int foreign key references M_SalesPerson(SalePSID) )  Chèn dữ liệu cho hai bảng trên lấy từ SalesPerson và SalesOrderHeader chọn 
+-- 		SalesOrdID int not null primary key, 
+-- 		OrderDate date,
+-- 		SubTotalOrd money,
+-- 		SalePSID int foreign key references M_SalesPerson(SalePSID) 
+-- ) 
+-- - Chèn dữ liệu cho hai bảng trên lấy từ SalesPerson và SalesOrderHeader chọn 
 -- những field tương ứng với 2 bảng mới tạo.
---  Viết trigger cho thao tác insert trên bảng M_SalesOrderHeader, khi trigger 
+-- - Viết trigger cho thao tác insert trên bảng M_SalesOrderHeader, khi trigger 
 -- thực thi thì dữ liệu trong bảng M_SalesPerson được cập nhật.
+
+create table M_SalesPerson
+(
+	SalePSID int not null primary key,
+	TerritoryID int,
+	BonusPS money
+)
+
+create table M_SalesOrderHeader
+(
+	SalesOrdID int not null primary key,
+	OrderDate date,
+	SubTotalOrd money,
+	SalePSID int foreign key references M_SalesPerson(SalePSID)
+)
+
+insert into M_SalesPerson
+select s.BusinessEntityID, s.TerritoryID, s.Bonus
+from Sales.SalesPerson as s
+
+insert into M_SalesOrderHeader
+select s.SalesOrderID, s.OrderDate, s.SubTotal, s.SalesPersonID
+from Sales.SalesOrderHeader as s
+go
+
+CREATE trigger bonus_trigger on M_SalesOrderHeader
+FOR INSERT
+as
+	begin
+	DECLARE @doanhThu float, @maNV int
+	select @maNV= i.SalePSID
+	from inserted i
+
+	set @doanhThu=(
+		select sum([SubTotalOrd])
+	from [dbo].[M_SalesOrderHeader]
+	where SalePSID=@maNV
+	)
+
+	if (@doanhThu > 10000000)
+	begin
+		update M_SalesPerson
+				set BonusPS += BonusPS * 0.1
+				where SalePSID=@maNV
+	end
+end
+go
